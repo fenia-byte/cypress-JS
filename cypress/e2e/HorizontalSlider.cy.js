ddf512cba637ceaa6d75cba727b14da7d1f23adb
Fenia <fenyakhachatrian@gmail.com>
2025-01-16 19:04:14 +0400

    horozontal

--- cypress/e2e/HorizontalSlider.cy.js
+++ cypress/e2e/HorizontalSlider.cy.js
@@ -1,81 +1,61 @@
-import { horizontalSlider } from "..Pages/HorizontalSlider";
-import { basePage } from "..Pages/Base";
-import { LOCATORS } from "..utils/locators";
+import { horizontalSlider } from "../Pages/HorizontalSlider";
+import { basePage } from "../Pages/Base";
+import { LOCATORS } from "../utils/locators";
 
-describe("horizontalSlider", () => {
-  beforeEach(() => {
+describe("Horizontal Slider", () => {
+  beforeEach(() => {            
     cy.visit("/");
     cy.contains(horizontalSlider.NAMES.contentText).click();
   });
 
-  it("Verify link and content", () => {
-    cy.url().should("include", `${horizontalSlider.NAMES.horizSliderLinkName}`);
+  it("should verify the link and content", () => {
+    cy.url().should("include", horizontalSlider.NAMES.horizSliderLinkName);
 
     basePage
       .getExample()
-      .get("h3")
+      .find("h3")
       .should("contain", horizontalSlider.NAMES.header);
+
     basePage
       .getExample()
-      .get("h4")
+      .find("h4")
       .should("contain", horizontalSlider.NAMES.description);
   });
 
-  it("Verify the deafault, min, max, step values of the slider in html", () => {
-    horizontalSlider.getSliderContainer().scrollIntoView().should("be.visible"); //element is visible
+  it("should verify the default, min, max, and step values of the slider in HTML", () => {
+    horizontalSlider.getSliderContainer().scrollIntoView().should("be.visible");
 
-    horizontalSlider.getSliderValue().then(($span) => {
-      const spanText = $span.text();
-      expect(spanText).to.equal("0"); // Check the default value is 0
-    });
+    horizontalSlider.getSliderValue().should("have.text", "0");
 
-    horizontalSlider.getSliderAttr().then(($input) => {
-      // Get the "min","max", "Step", "Value" attribute from the input element
-      const minValue = $input.attr("min");
-      const maxValue = $input.attr("max");
-      const stepValue = $input.attr("step");
-      const valValue = $input.attr("value");
-      // Assert that the min, max, step, value values is what you expect in HTML
-      expect(minValue).to.equal("0.0");
-      expect(maxValue).to.equal("5.0");
-      expect(stepValue).to.equal("0.5");
-      expect(valValue).to.equal("0");
+    horizontalSlider.getSliderAttr().should(($input) => {
+      expect($input.attr("min")).to.equal("0.0");
+      expect($input.attr("max")).to.equal("5.0");
+      expect($input.attr("step")).to.equal("0.5");
+      expect($input.attr("value")).to.equal("0");
     });
   });
-  it("Verify the min, max, step values of the slider, and that step is done correctly in GUI", () => {
-    horizontalSlider.getSliderContainer().scrollIntoView().should("be.visible"); //element is visible
 
-    // move to 4 and check the value
+  it("should verify the slider's behavior and step functionality in the GUI", () => {
+    horizontalSlider.getSliderContainer().scrollIntoView().should("be.visible");
+
     horizontalSlider.getSliderAttr().invoke("val", 4).trigger("change");
-
-    horizontalSlider
-      .getSliderContainer()
-      .get("#range")
-      .then(($span) => {
-        const spanText = parseFloat($span.text());
-        expect(spanText).to.equal(4); // Check the value
-      });
+    horizontalSlider.getSliderValue().should(($span) => {
+      expect(parseFloat($span.text())).to.equal(4);
+    });
 
     horizontalSlider.getSliderAttr().invoke("val", 10).trigger("change");
-    horizontalSlider.getSliderValue().then(($span) => {
-      const spanText = parseFloat($span.text());
-      expect(spanText).to.not.equal(10); // Check max
-      expect(spanText).to.equal(5); // Check max
+    horizontalSlider.getSliderValue().should(($span) => {
+      expect(parseFloat($span.text())).to.equal(5);
     });
 
     horizontalSlider.getSliderAttr().invoke("val", -1).trigger("change");
-    horizontalSlider.getSliderValue().then(($span) => {
-      const spanText = parseFloat($span.text());
-      expect(spanText).to.not.equal(-1); // Check min
-      expect(spanText).to.equal(0); // Check min
+    horizontalSlider.getSliderValue().should(($span) => {
+      expect(parseFloat($span.text())).to.equal(0);
     });
 
-    //check step
     horizontalSlider.getSliderAttr().invoke("val", 0.55).trigger("change");
-    horizontalSlider.getSliderValue().then(($span) => {
-      const spanText = parseFloat($span.text());
-      expect(spanText).to.not.equal(0.55); // Check step
-      expect(spanText).to.equal(0.5); // Check step
+    horizontalSlider.getSliderValue().should(($span) => {
+      expect(parseFloat($span.text())).to.equal(0.5);
     });
   });
 });
